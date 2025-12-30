1579bf7c519cde8640edb0f69ff8581f0d36a41e
040772 <salomonguttmann@gmail.com>
2025-12-30 16:04:43 +0100

    add implementation to use curl in app

--- src/main/java/com/rest/webservices/restfulwebservices/campain/CampainJpaResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/campain/CampainJpaResource.java
@@ -1,8 +1,12 @@
 package com.rest.webservices.restfulwebservices.campain;
 
-//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import com.rest.webservices.restfulwebservices.jpa.CampainRepository;
+import com.rest.webservices.restfulwebservices.writexmlrq.GetCampaignSoapRequestBuilder;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -21,10 +26,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class CampainJpaResource {
 
     private CampainRepository campainRepository;
+    private GetCampaignSoapRequestBuilder getCampaignSoapRequestBuilder;
 
-    public CampainJpaResource(CampainRepository campainRepository)
-    {
+    public CampainJpaResource(CampainRepository campainRepository, GetCampaignSoapRequestBuilder getCampaignSoapRequestBuilder) {
         this.campainRepository = campainRepository;
+        this.getCampaignSoapRequestBuilder = getCampaignSoapRequestBuilder;
     }
 
     @GetMapping("/jpa/campains")
@@ -34,7 +40,7 @@ public class CampainJpaResource {
     }
 
     @GetMapping("/jpa/campains/{id}")
-    public EntityModel<MTImportCampainReq>  retrieveCampain(@PathVariable int id) {
+    public EntityModel<MTImportCampainReq> retrieveCampain(@PathVariable int id) {
 
         Optional<MTImportCampainReq> mtImportCampainReq = campainRepository.findById(id);
 
@@ -45,7 +51,7 @@ public class CampainJpaResource {
         //wrapping the user class
         EntityModel<MTImportCampainReq> entityModel = EntityModel.of(mtImportCampainReq.get());
         //crate a link
-        WebMvcLinkBuilder link =  linkTo(methodOn(this.getClass()).retrieveAllCampains());
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllCampains());
         //add the link
         entityModel.add(link.withRel("all-canpains"));
 
@@ -58,12 +64,48 @@ public class CampainJpaResource {
     }
 
     @PostMapping("/jpa/campains")
-    public ResponseEntity<MTImportCampainReq> createCampain(@Valid @RequestBody MTImportCampainReq mtImportCampainReq) {
-        MTImportCampainReq savedCampain=campainRepository.save(mtImportCampainReq);
+    public ResponseEntity<MTImportCampainReq> createCampain(@Valid @RequestBody MTImportCampainReq mtImportCampainReq) throws Exception {
+        MTImportCampainReq savedCampain = campainRepository.save(mtImportCampainReq);
+
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedCampain.getId())
                 .toUri();
+
+        stringToDom(getCampaignSoapRequestBuilder.generateXml(mtImportCampainReq));
+
+        /**CURL vorbereiten und starten um SOAP REQ/RES an M-PROMO zu simulieren*/
+        try {
+            String command =
+                    "curl --header \"Content-Type: text/xml\" -d @request.xml http://localhost:8080/services";
+
+            ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+            processBuilder.directory(new File("testSOAP/"));
+
+            Process process = processBuilder.start();
+
+            InputStream inputStream = process.getInputStream();
+
+            for (int i = 0; i < inputStream.available(); i++) {
+                System.out.println("" + inputStream.read());
+            }
+
+            Thread.sleep(10000);
+            process.destroy();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        /***********************CURL ende********************************************************/
+
         return ResponseEntity.created(location).build();
+    }
+
+    public static void stringToDom(String xmlSource)
+            throws IOException {
+        java.io.FileWriter fw = new java.io.FileWriter("testSOAP/request.xml");
+        fw.write(xmlSource);
+        fw.close();
     }
 }
--- src/main/java/com/rest/webservices/restfulwebservices/soap/ObjectFactory.java
+++ src/main/java/com/rest/webservices/restfulwebservices/soap/ObjectFactory.java
@@ -11,18 +11,17 @@ import jakarta.xml.bind.annotation.XmlRegistry;
 
 
 /**
- * This object contains factory methods for each 
- * Java content interface and Java element interface 
- * generated in the com.rest.webservices.restfulwebservices.soap package. 
- * <p>An ObjectFactory allows you to programmatically 
- * construct new instances of the Java representation 
- * for XML content. The Java representation of XML 
- * content can consist of schema derived interfaces 
- * and classes representing the binding of schema 
- * type definitions, element declarations and model 
- * groups.  Factory methods for each of these are 
+ * This object contains factory methods for each
+ * Java content interface and Java element interface
+ * generated in the com.rest.webservices.restfulwebservices.soap package.
+ * <p>An ObjectFactory allows you to programmatically
+ * construct new instances of the Java representation
+ * for XML content. The Java representation of XML
+ * content can consist of schema derived interfaces
+ * and classes representing the binding of schema
+ * type definitions, element declarations and model
+ * groups.  Factory methods for each of these are
  * provided in this class.
- * 
  */
 @XmlRegistry
 public class ObjectFactory {
@@ -30,129 +29,125 @@ public class ObjectFactory {
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.rest.webservices.restfulwebservices.soap
-     * 
      */
     public ObjectFactory() {
     }
 
     /**
-     * Create an instance of {@link Campain }
-     * 
-     * @return
-     *     the new instance of {@link Campain }
-     */
-    public Campain createCampain() {
-        return new Campain();
-    }
-
-    /**
      * Create an instance of {@link GetCampainRequest }
-     * 
-     * @return
-     *     the new instance of {@link GetCampainRequest }
+     *
+     * @return the new instance of {@link GetCampainRequest }
      */
     public GetCampainRequest createGetCampainRequest() {
         return new GetCampainRequest();
     }
 
     /**
+     * Create an instance of {@link CampainRes }
+     *
+     * @return the new instance of {@link CampainRes }
+     */
+    public CampainRes createCampainRes() {
+        return new CampainRes();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.KampagneName }
+     *
+     * @return the new instance of {@link GetCampainRequest.KampagneName }
+     */
+    public GetCampainRequest.KampagneName createGetCampainRequestKampagneName() {
+        return new GetCampainRequest.KampagneName();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Beschreibung }
+     *
+     * @return the new instance of {@link GetCampainRequest.Beschreibung }
+     */
+    public GetCampainRequest.Beschreibung createGetCampainRequestBeschreibung() {
+        return new GetCampainRequest.Beschreibung();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Zuordnungsbereiche }
+     *
+     * @return the new instance of {@link GetCampainRequest.Zuordnungsbereiche }
+     */
+    public GetCampainRequest.Zuordnungsbereiche createGetCampainRequestZuordnungsbereiche() {
+        return new GetCampainRequest.Zuordnungsbereiche();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Organisationen }
+     *
+     * @return the new instance of {@link GetCampainRequest.Organisationen }
+     */
+    public GetCampainRequest.Organisationen createGetCampainRequestOrganisationen() {
+        return new GetCampainRequest.Organisationen();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Industrien }
+     *
+     * @return the new instance of {@link GetCampainRequest.Industrien }
+     */
+    public GetCampainRequest.Industrien createGetCampainRequestIndustrien() {
+        return new GetCampainRequest.Industrien();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Marken }
+     *
+     * @return the new instance of {@link GetCampainRequest.Marken }
+     */
+    public GetCampainRequest.Marken createGetCampainRequestMarken() {
+        return new GetCampainRequest.Marken();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Label }
+     *
+     * @return the new instance of {@link GetCampainRequest.Label }
+     */
+    public GetCampainRequest.Label createGetCampainRequestLabel() {
+        return new GetCampainRequest.Label();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Kundengruppen }
+     *
+     * @return the new instance of {@link GetCampainRequest.Kundengruppen }
+     */
+    public GetCampainRequest.Kundengruppen createGetCampainRequestKundengruppen() {
+        return new GetCampainRequest.Kundengruppen();
+    }
+
+    /**
+     * Create an instance of {@link GetCampainRequest.Profilierungssortimente }
+     *
+     * @return the new instance of {@link GetCampainRequest.Profilierungssortimente }
+     */
+    public GetCampainRequest.Profilierungssortimente createGetCampainRequestProfilierungssortimente() {
+        return new GetCampainRequest.Profilierungssortimente();
+    }
+
+    /**
      * Create an instance of {@link GetCampainResponse }
-     * 
-     * @return
-     *     the new instance of {@link GetCampainResponse }
+     *
+     * @return the new instance of {@link GetCampainResponse }
      */
     public GetCampainResponse createGetCampainResponse() {
         return new GetCampainResponse();
     }
 
     /**
-     * Create an instance of {@link Campain.KampagneName }
-     * 
-     * @return
-     *     the new instance of {@link Campain.KampagneName }
+     * Create an instance of {@link CampainRes.Message }
+     *
+     * @return the new instance of {@link CampainRes.Message }
      */
-    public Campain.KampagneName createCampainKampagneName() {
-        return new Campain.KampagneName();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Beschreibung }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Beschreibung }
-     */
-    public Campain.Beschreibung createCampainBeschreibung() {
-        return new Campain.Beschreibung();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Zuordnungsbereiche }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Zuordnungsbereiche }
-     */
-    public Campain.Zuordnungsbereiche createCampainZuordnungsbereiche() {
-        return new Campain.Zuordnungsbereiche();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Organisationen }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Organisationen }
-     */
-    public Campain.Organisationen createCampainOrganisationen() {
-        return new Campain.Organisationen();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Industrien }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Industrien }
-     */
-    public Campain.Industrien createCampainIndustrien() {
-        return new Campain.Industrien();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Marken }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Marken }
-     */
-    public Campain.Marken createCampainMarken() {
-        return new Campain.Marken();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Label }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Label }
-     */
-    public Campain.Label createCampainLabel() {
-        return new Campain.Label();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Kundengruppen }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Kundengruppen }
-     */
-    public Campain.Kundengruppen createCampainKundengruppen() {
-        return new Campain.Kundengruppen();
-    }
-
-    /**
-     * Create an instance of {@link Campain.Profilierungssortimente }
-     * 
-     * @return
-     *     the new instance of {@link Campain.Profilierungssortimente }
-     */
-    public Campain.Profilierungssortimente createCampainProfilierungssortimente() {
-        return new Campain.Profilierungssortimente();
+    public CampainRes.Message createCampainResMessage() {
+        return new CampainRes.Message();
     }
 
 }
